f70e474674575efca7bd0c6272d514ac7ab50dce
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-09-11 14:40:50 +0530

    Add: home content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -17,8 +17,20 @@ const Home = () => {
       {snap.intro && (
         <motion.section className='home' {...slideAnimation('left')}>
           <motion.header>
-            <img src='./logo-black.png' alt='logo' className='w-25 h-25 object-contain'/>
+            <img src='./logo-black.png' alt='logo' className='w-10 h-10 object-contain'/>
           </motion.header>
+
+          <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>It's <br className='xl:block hidden'/>Free.</h1>
+            </motion.div>
+            <motion.div>
+              <p className='max-w-md'>A <strong>COMMUNITY</strong> driven Platform to create amazing 3d mockups.
+               It's free, Consisting of Various 3d modals for you to select from.</p>
+            </motion.div>
+            
+          </motion.div>
+
         </motion.section>
       )}
     </AnimatePresence>
