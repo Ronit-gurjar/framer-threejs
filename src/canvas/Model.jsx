27c05ee7889644d871f60868b6e0a730f3406513
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-09-11 18:46:26 +0530

    Add: mesh properties

--- src/canvas/Model.jsx
+++ src/canvas/Model.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React,{useRef} from 'react'
 import {easing} from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame} from '@react-three/fiber';
 import { Decal, useGLTF, useTexture} from '@react-three/drei';
 import state from '../store'
 
-const Model = () => {
+const Model = (props) => {
+  const meshRef = useRef();
   const snap = useSnapshot(state);
   const { nodes, materials } = useGLTF('/shirt_baked.glb');
 
@@ -14,11 +15,14 @@ const Model = () => {
   return (
     <group>
       <mesh
+        ref={meshRef}
+        receiveShadow
         castShadow
         geometry={nodes.T_Shirt_male.geometry}
         material={materials.lambert1}
         material-roughness={1}
         dispose={null}
+        {...props}
       >
       </mesh>
     </group>
