3f18889efb65a16ec353bd6dcf6fc2be0848bdb4
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-09-11 18:45:26 +0530

    Fix: create custom lightning <issue resolved>

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,20 +1,38 @@
 import { Canvas } from "@react-three/fiber";
-import { Environment, Center } from "@react-three/drei";
+import { Environment, Center, OrbitControls } from "@react-three/drei";
+import * as THREE from 'three';
 
 import Model from './Model';
 import CameraRig from './CameraRig';
 import Backdrop from './Backdrop';
 
 const CanvasModel = () => {
+
   return (
-    <Canvas>
+    <Canvas shadowMap camera={{ position: [0, 5, 10] }}>
       <ambientLight intensity={0.7}/>
+
+       {/* Create a directional light */}
+       <directionalLight
+        position={[0, 10, 0]}
+        intensity={1}
+        castShadow={true}
+        shadow-mapSize-width={1024}
+        shadow-mapSize-height={1024}
+        shadow-camera-near={0.5}
+        shadow-camera-far={500}
+        shadow-camera-top={50}
+        shadow-camera-bottom={-50}
+        shadow-camera-left={-50}
+        shadow-camera-right={50}
+      />
       <CameraRig>
         
           <Center>
             <Model/>
           </Center>
       </CameraRig>
+      <OrbitControls />
     </Canvas>
   )
 }
