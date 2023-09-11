c2e4e7dc509373dee95827852c48accd88db17d3
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-09-11 15:44:50 +0530

    Add: imports

--- src/pages/Customize.jsx
+++ src/pages/Customize.jsx
@@ -1,4 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+import state from '../store';
+import { download } from '../assets';
+import { donwloadCanvasToImage, reader } from '../config/helpers';
+import {EditorTabs, FilterTabs, DecalTypes } from '../config/constants';
+import {fadeAnimation, slideAnimation} from '../config/motion';
+
+import { FilePicker, ModelPicker, ModelColorPicker, Tab } from '../components';
 
 const Customize = () => {
   return (
