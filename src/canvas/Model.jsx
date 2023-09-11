1bce512245626d3aea843589fbf1c2b2ef41c902
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-09-11 16:50:10 +0530

    Add: imports

--- src/canvas/Model.jsx
+++ src/canvas/Model.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {easing} from 'maath';
+import { useSnapshot } from 'valtio';
+import { useFrame} from '@react-three/fiber';
+import { Decal, useGLTF, useTexture} from '@react-three/drei';
 
 const Model = () => {
+  const snap = useSnapshot(state)
   return (
     <div>Model</div>
   )
