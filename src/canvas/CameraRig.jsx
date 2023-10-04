e417f64af74e508bda95ad2b8543ed27ff56582a
Ronit-gurjar <ronitgurjar999@gmail.com>
2023-10-04 11:26:40 +0530

    Add: initial positions for camera and model

--- src/canvas/CameraRig.jsx
+++ src/canvas/CameraRig.jsx
@@ -6,6 +6,29 @@ import state from '../store'
 
 const CameraRig = ({children}) => {
   const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) =>{
+    const isBreakpoint = window.innerWidth <= 1260;
+    const isMobile = window.innerWidth <= 600;
+   
+    //set initial position model
+    let targetPosition = [-0.4,0,2]; //[x,y,z]
+    if(snap.intro) {
+        if(isBreakpoint) targetPosition = [0, 0, 2];
+        if (isMobile) targetPosition = [-0.6, 1, 2.5];
+      } else {
+        if(isMobile) targetPosition = [0, 0, 2.5]
+        else targetPosition = [0, 0, 2];
+      }
+
+    //set initial camera position
+    easing.damp3(state.camera.position, targetPosition,0.30, delta)
+
+    //set model rotation smooth
+    easing.dampC(group.current.rotation,[state.pointer.y / 10, -state.pointer.x / 5, 0],0.25,delta)
+  })
+
   return (
     <group ref={group}>{children}</group>
   )
